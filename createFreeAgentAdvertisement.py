ce213212f0b93d036008b5a32e36f56fd60e63e2
iXeroll <evaldas1999@gmail.com>
2021-04-05 19:07:57 +0300

    First version of createFreeAgentAdvertisement

---
+++ createFreeAgentAdvertisement.py
@@ -0,0 +1,7 @@
+
+
+def createAdvertisement(name,game,rank,description):
+    result=('Name: {0} Game: {1} Rank: {2} \nDescription: {3} '.format(name,game,rank,description))
+    return result
+
+#print(createAdvertisement("iXeroll","Valorant","Immortal","Looking for a Comp Lithuanian Team"))
