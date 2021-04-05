a0c8e9c9f98bb0b8f380c6b0fc4f6b7bce9b76d3
mageris225 <76109741+mageris225@users.noreply.github.com>
2021-04-06 01:38:30 +0300

    Full login function.
    Login function logic created still needs unit tests.

--- login.java
+++ login.java
@@ -1,12 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class login {
 
     String username;
     String password;
 
-    void login(){
+    void login() throws FileNotFoundException {
+        username = askForUsername();
+        password = askForPass();
 
-        if(//if user exsists){
-            //login to system
+        if (userExsists(username, password)) {
+            //loginTo;
         }
     }
+
+    static String askForUsername(){
+        System.out.println("Gib user");
+        Scanner scanner = new Scanner("data.txt");
+        return scanner.nextLine();
+
+    }
+
+    static String askForPass(){
+        System.out.println("Gib pass");
+        Scanner scanner = new Scanner("data.txt");
+        return scanner.nextLine();
+    }
+
+    boolean userExsists(String username, String password) throws FileNotFoundException {
+
+        File myObj = new File("data.txt");
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            String dataUsername = data.substring(data.indexOf(":") + 1, data.lastIndexOf(":"));
+            String dataPassword = data.substring(data.lastIndexOf(":") + 1);
+            if (dataUsername.equals(username) && (dataPassword.equals(dataPassword))) {
+                myReader.close();
+                return true;
+            }
+
+        }
+        myReader.close();
+        return false;
+
+    }
+
+
 }
