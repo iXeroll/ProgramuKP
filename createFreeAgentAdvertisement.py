ee9808dc4c39ef7930bff8bcba2aca9fc16c3024
iXeroll <evaldas1999@gmail.com>
2021-04-05 23:02:28 +0300

    Added a fail error if a required field is empty

--- createFreeAgentAdvertisement.py
+++ createFreeAgentAdvertisement.py
@@ -1,7 +1,11 @@
 
 
 def createAdvertisement(name,game,rank,description):
+    
+    if (name== "" or game== "" or rank== "" or description == ""):
+        return "One or more required fields are empty"
+    
     result=('Name: {0} Game: {1} Rank: {2} \nDescription: {3} '.format(name,game,rank,description))
     return result
 
-#print(createAdvertisement("iXeroll","Valorant","Immortal","Looking for a Comp Lithuanian Team"))
+print(createAdvertisement("iXeroll","Valorant","Immortal","Looking for a Comp Lithuanian Team"))
