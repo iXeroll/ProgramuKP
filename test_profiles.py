f0cfc92127326111ac5688642ee6cce4121d70f9
KavaBeSielos <domantaskliukas@gmail.com>
2021-04-05 23:36:09 +0300

    added negative test and error test

--- test_profiles.py
+++ test_profiles.py
@@ -3,8 +3,16 @@ import Profiles
 
 class testProfile(unittest.TestCase):
 
-    def test_profilesave(self):
+    def test_profilesave_possitive(self):
          gameProfile = Profiles.gameProfile("Archeage")
+         self.assertEqual(gameProfile.selected_game(), "Archeage")
+
+    def test_profilesave_negative(self):
+         gameProfile = Profiles.gameProfile("Archeage")
+         self.assertEqual(gameProfile.selected_game(), "archeage")
+
+    def test_profilesave_error(self):
+         gameProfile = Profiles.gameProfile(gamename)
          self.assertEqual(gameProfile.selected_game(), "Archeage")
 
 if __name__ == '__main__':
