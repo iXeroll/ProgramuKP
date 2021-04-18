9d8cd6fede84329dea41fcf08679bc09c291094b
IgnasSteponenas <50793829+IgnasSteponenas@users.noreply.github.com>
2021-04-18 22:18:34 +0300

    Add files via upload
    gradle

---
+++ src/test/java/RegistrationTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationTest {
+
+    @Test
+    void ShouldFindIfUsernameIsUnique() throws FileNotFoundException {
+        boolean test = Registration.usernameIsUnique("test");
+
+        Assert.assertEquals(false, test);
+    }
+
+    @Test
+    void ShouldFindIfUsernameIsUnique2() throws FileNotFoundException {
+        boolean test = Registration.usernameIsUnique("test");
+
+        Assert.assertEquals(true, test);
+    }
+
+    @Test
+    void ShouldFindIfUsernameIsUnique3() throws FileNotFoundException {
+        boolean test = Registration.usernameIsUnique("test2");
+
+        Assert.assertEquals(false, test);
+    }
+}
